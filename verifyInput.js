cd2bef7dd137e7918929479166e9de5d385a68f0
Carlos Lopez <carlosmlopez93@gmail.com>
2019-07-05 14:46:26 -0700

    added form sheet for 642

---
+++ verifyInput.js
@@ -0,0 +1,18 @@
+var checkr = document.forms.form;
+var message = document.getElementById("message");
+
+checkr.onsubmit = function () {
+
+    if (checkr.first.value > 60) {
+        message.innerHTML("try a shorter name");
+        return false;
+    }
+    if (checkr.first.value == "") {
+        message.innerHTML = "please enter a first name";
+        return false;
+    } else {
+        message.innerHTML = "";
+        return true;
+    }
+
+};
