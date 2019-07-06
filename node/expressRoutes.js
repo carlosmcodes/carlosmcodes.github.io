743f82586892ef7909abf7ab42a17f8238263db7
Carlos Lopez <carlosmlopez93@gmail.com>
2019-07-06 12:08:19 -0700

    injection of views ejs

--- node/expressRoutes.js
+++ node/expressRoutes.js
@@ -3,33 +3,42 @@ const app = express();
 app.set('view engine', 'ejs');
 
 
-// helps solve traversing up tree
-const path = require('path');
-const refinePath = path.join(__dirname, '/../');
-
-
 //hard coded
+// app.get('/', function(req,res){
+//     res.render('index');
+
+// });
+// app.get('/form', function(req,res){
+//     res.render('formSheet');
+
+
+// });
+
+
+// app.get('/404', function(req,res){
+//     res.render('404');
+
+
+// });
+
+
 app.get('/', function(req,res){
-    res.sendFile(refinePath + 'index.html');
+    res.render('profile');
+
 
 });
-app.get('/form', function(req,res){
-    res.sendFile(refinePath + 'form/formSheet.html');
-
-});
-
 
 // the :<var> is what gets the func to work
-app.get('/:name', function(req,res){
-    res.send(`requested website value: ${req.params.name}`);
-});
+// app.get('/:name', function(req,res){
+//     res.sendFile(`requested website value: ${req.params.name}`);
+// });
 
 
-app.get('/profile/:name', function(req,res){
-    var data = {age: 26, job: 'scoot!', hobbies: ['rugby', 'sports', 'beach days']}
-    res.render('profile', 
-    {person: req.params.name, data: data});
-});
+// app.get('/profile/:name', function(req,res){
+//     var data = {age: 26, job: 'scoot!', hobbies: ['rugby', 'sports', 'beach days']}
+//     res.render('profile', 
+//     {person: req.params.name, data: data});
+// });
 
 //output
 app.listen(3000);
